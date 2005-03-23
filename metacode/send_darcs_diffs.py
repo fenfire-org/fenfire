745cda9b215ab0aca64e571ca3ab92d181459078
Benja Fallenstein <b.fallenstein@gmx.de>
2005-03-23 21:54:46 +0200

    add copyright statement to send_darcs_diffs.py
    darcs-hash:20050323195446-2ee97-efb4888132fc2a53bc4bd7113c8d9ef3c0723aa5

--- metacode/send_darcs_diffs.py
+++ metacode/send_darcs_diffs.py
@@ -1,4 +1,23 @@
-# (c): Benja Fallenstein and Matti J. Katila
+# 
+# Copyright (c) 2005, Benja Fallenstein and Matti J. Katila
+# This file is part of metacode/.
+# 
+# metacode/ is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# metacode/ is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+# 
+# You should have received a copy of the GNU General
+# Public License along with metacode/; if not, write to the Free
+# Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA  02111-1307  USA
+# 
+
 
 """
 Program that sends a source diff to the mailing-list.
