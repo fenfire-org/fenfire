b33c56680384a57609448c41c1697733274cad38
Benja Fallenstein <b.fallenstein@gmx.de>
2005-04-13 12:37:37 +0300

    restructure PropertyListContentView code to be extensible
    darcs-hash:20050413093737-2ee97-0495e1d27a242585f8c77517730a7d87187b4a44

---
+++ org/fenfire/view/content/Viewlet.java
@@ -0,0 +1,41 @@
+/* 
+Viewlet.java
+ *
+ *    Copyright (c) 2005 by Benja Fallenstein
+ *
+ *    This file is part of Fenfire.
+ *    
+ *    Fenfire is free software; you can redistribute it and/or modify it under
+ *    the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *    
+ *    Fenfire is distributed in the hope that it will be useful, but WITHOUT
+ *    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ *    Public License for more details.
+ *    
+ *    You should have received a copy of the GNU General
+ *    Public License along with Fenfire; if not, write to the Free
+ *    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *    MA  02111-1307  USA
+ *
+ */
+/*
+ * Written by Benja Fallenstein
+ */
+package org.fenfire.view.content;
+import org.fenfire.Cursor;
+import org.fenfire.view.*;
+import org.fenfire.swamp.*;
+import org.nongnu.libvob.lob.*;
+import org.nongnu.libvob.lob.lobs.*;
+import javolution.realtime.*;
+import java.awt.Color;
+import java.util.*;
+
+public interface Viewlet extends Realtime {
+
+    public Lob getCaption();
+    public Lob getLob(Cursor cursor, Object node, int dir);
+}
